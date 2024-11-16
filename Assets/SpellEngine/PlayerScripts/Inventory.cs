87a290ed1fb933a620542fa1471915cd2be23ddb
StrawberryBalloons <callumganter@yahoo.co.uk>
2024-11-16 20:45:45 +0000

    Rearranged a bunch of things, deleted others and added imports for UI elements, added an inventory UI

---
+++ Assets/SpellEngine/PlayerScripts/Inventory.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public static Inventory instance;
+
+    public List<Item> items = new List<Item>();
+
+    public delegate void OnItemChanged();
+    public OnItemChanged onItemChangedCallback;
+    public int space = 20;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("There is more than one instance of inventory detected");
+            return;
+        }
+        instance = this;
+    }
+
+    public bool Add(Item item)
+    {
+        if (!item.isDefault)
+        {
+            if (items.Count >= space)
+            {
+                Debug.Log("Not enough Inven space");
+                return false;
+            }
+            items.Add(item);
+
+            if (onItemChangedCallback != null)
+            {
+                onItemChangedCallback.Invoke();
+            }
+        }
+        return true;
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+
+        if (onItemChangedCallback != null)
+        {
+            onItemChangedCallback.Invoke();
+        }
+    }
+
+
+}
